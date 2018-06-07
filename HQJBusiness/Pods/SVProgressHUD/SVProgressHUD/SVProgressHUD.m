fac8947473a257eadab5824096f2f75c5805d64d
谢增文 <xiezw94@126.com>
2018-06-07 16:02:42 +0800

    重新加了第三方加载框

--- HQJBusiness/Pods/SVProgressHUD/SVProgressHUD/SVProgressHUD.m
+++ HQJBusiness/Pods/SVProgressHUD/SVProgressHUD/SVProgressHUD.m
@@ -1171,7 +1171,7 @@ static const CGFloat SVProgressHUDLabelSpacing = 8.0f;
 #pragma mark - Getters
 
 + (NSTimeInterval)displayDurationForString:(NSString*)string {
-    CGFloat minimum = MIN((CGFloat)string.length * 0.02 + 0.5, [self sharedView].minimumDismissTimeInterval);
+    CGFloat minimum = MAX((CGFloat)string.length * 0.06 + 0.5, [self sharedView].minimumDismissTimeInterval);
     return MIN(minimum, [self sharedView].maximumDismissTimeInterval);
 }
 
