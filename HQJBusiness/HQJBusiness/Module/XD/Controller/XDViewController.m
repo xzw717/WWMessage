b72bc841851122e1f46a562accbc70159f8f6dde
谢增文 <xiezw94@126.com>
2020-05-18 09:48:29 +0800

    增加XD板块

---
+++ HQJBusiness/HQJBusiness/Module/XD/Controller/XDViewController.m
@@ -0,0 +1,32 @@
+//
+//  XDViewController.m
+//  HQJBusiness
+//
+//  Created by mymac on 2020/5/17.
+//  Copyright © 2020 Fujian first time iot technology investment co., LTD. All rights reserved.
+//
+
+#import "XDViewController.h"
+
+@interface XDViewController ()
+
+@end
+
+@implementation XDViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
