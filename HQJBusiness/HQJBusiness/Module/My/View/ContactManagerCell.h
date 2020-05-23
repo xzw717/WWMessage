9744dcd75f908b224b8962b0766b9bc7fe1594f3
谢增文 <xiezw94@126.com>
2020-05-23 16:19:01 +0800

    cell

---
+++ HQJBusiness/HQJBusiness/Module/My/View/ContactManagerCell.h
@@ -0,0 +1,17 @@
+//
+//  XDShopTableViewCell.h
+//  HQJBusiness
+//
+//  Created by mymac on 2020/5/19.
+//  Copyright © 2020 Fujian first time iot technology investment co., LTD. All rights reserved.
+//
+
+#import "ZW_TableViewCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContactManagerCell : ZW_TableViewCell
+- (void)setTitle:(NSString *)title;
+@end
+
+NS_ASSUME_NONNULL_END
