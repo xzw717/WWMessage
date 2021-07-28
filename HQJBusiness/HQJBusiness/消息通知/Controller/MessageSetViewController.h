75e83f68a74236215799b463457c95ffe6d840f6
谢增文 <xiezw94@126.com>
2021-07-28 15:09:38 +0800

    消息设置界面创建

---
+++ HQJBusiness/HQJBusiness/消息通知/Controller/MessageSetViewController.h
@@ -0,0 +1,17 @@
+//
+//  MessageSetViewController.h
+//  HQJBusiness
+//
+//  Created by Ethan on 2021/7/28.
+//  Copyright © 2021 Fujian first time iot technology investment co., LTD. All rights reserved.
+//
+
+#import "ZW_ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MessageSetViewController : ZW_ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HQJBusiness/HQJBusiness/消息通知/View/MessageSetCell.h
@@ -0,0 +1,17 @@
+//
+//  MessageSetCell.h
+//  HQJBusiness
+//
+//  Created by Ethan on 2021/7/28.
+//  Copyright © 2021 Fujian first time iot technology investment co., LTD. All rights reserved.
+//
+
+#import "ZW_TableViewCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MessageSetCell : ZW_TableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
